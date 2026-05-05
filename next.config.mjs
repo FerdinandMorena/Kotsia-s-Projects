42b8ae4d7916eb4250d97ae3ad483b23f41283e3
Ferdinand <fmmphahle01@gmail.com>
2026-05-05 10:59:05 +0200

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  typescript: {},
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
